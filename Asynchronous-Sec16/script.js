4b9436535c7278b20febbbdc0e325e4c9b3f0caf
Ashwini Patil <ashwiniaski23@gmail.com>
2025-06-02 10:39:45 -0400

    Adding building/creating promises code

--- Asynchronous-Sec16/script.js
+++ Asynchronous-Sec16/script.js
@@ -341,6 +341,7 @@ btn.addEventListener('click', function () {
 // getCountryData('sdghjg');
 ////////////////////////////
 
+/*
 ////////////////////////
 // Event Loop in practice
 console.log('Test start');
@@ -352,6 +353,35 @@ Promise.resolve('Resolved promise 2').then(res => {
 });
 console.log('Test end');
 /////////////////
+*/
 
 //////////////////////
 // Building a Simple Promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+
+    console.log('Lottery draw is happening :)')
+    setTimeout(function () {
+        if (Math.random() >= 0.5) {
+            resolve('You win..');
+        } else {
+            reject(new Error('You lost your money :('));
+        }
+    }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+wait(2).then(() => {
+    console.log('I waited for 2 seconds..');
+    return wait(1);
+}).then(() => console.log('I waited for 1 second..'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('error')).catch(x => console.error(x));
