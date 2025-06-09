82ea831b3315a864d17bbc32dff4da6ddbb4e83a
Ashwini Patil <ashwiniaski23@gmail.com>
2025-06-09 14:37:42 -0400

    Adding initial code for JS-Fundamentals-1

---
+++ JS-Fundamentals-1/script.js
@@ -0,0 +1,12 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('JS is FUN!');
+
+console.log(40 + 23);
+*/
+
+// Values and Variables
+console.log('Jonas');
+console.log(24);
+let firstName = 'Jonas';
+console.log(firstName);
