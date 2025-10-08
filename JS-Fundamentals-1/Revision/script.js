9f56279b56bcf4cd78862173a34d5e570f2859d4
Ashwini <ashwiniaski23@gmail.com>
2025-10-08 15:35:32 -0400

    Commiting practice code

--- JS-Fundamentals-1/Revision/script.js
+++ JS-Fundamentals-1/Revision/script.js
@@ -1,5 +1,56 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
+// Values and Variables
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Ashwini");
+console.log(23);
+
+let firstName = "Ashwini";
+console.log(firstName);
+let PI = 3.142;
+
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2025;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+// let, const, var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+*/
+
+// Basic Operators: Airthematic, Comparision, Logical and Assignment
+const ageAshwini = 2037 - 1991;
+console.log(ageAshwini);
+
+const firstName = "Ashwini";
+const lastName = "Patil";
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+
+let x = 10 + 5;
+x += 10; // x=x+10
+console.log(x);
