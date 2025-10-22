01e6561b49b7b7adee31bc61d025e2b714630a93
Ashwini <ashwiniaski23@gmail.com>
2025-10-22 12:07:47 -0400

    Learnig developer skills and editor setup

--- DeveloperSkillsAndEditorSetup-Sec5/script.js
+++ DeveloperSkillsAndEditorSetup-Sec5/script.js
@@ -1,7 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 25;
-console.log(x);
+const xyz = 25;
+// console.log(x);
 console.log('Hello');
 console.log('Atharv');
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1991));
+console.log(xyz);
+console.log(`Hello 2025`);
