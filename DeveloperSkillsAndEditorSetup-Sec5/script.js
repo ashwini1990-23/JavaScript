387df1b11765dd23e35bd129fb0c5f6709e7f26d
Ashwini <ashwiniaski23@gmail.com>
2025-10-23 11:52:22 -0400

    Debugging skills ans Using AI

--- DeveloperSkillsAndEditorSetup-Sec5/script.js
+++ DeveloperSkillsAndEditorSetup-Sec5/script.js
@@ -11,3 +11,88 @@ const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1991));
 console.log(xyz);
 console.log(`Hello 2025`);
+
+//////////////////////////////////////
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
+// - What's a sensor error? And what do do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    // debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+////////////////////////
+// Debugging
+const measurementKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcicus',
+
+    // C) Fix a bug
+    // value: Number(prompt(`Degrees celcius: `)),
+    value: 10,
+  };
+
+  // B) Find a bug
+  console.log(measurement);
+  console.table(measurement);
+
+  console.log(typeof measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) Identify the bug
+console.log(measurementKelvin());
+
+////////////
+// Challenge 2
