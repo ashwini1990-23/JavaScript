56ab050c5ece513b1f7d87ce0b5db208997d1d0e
Ashwini <ashwiniaski23@gmail.com>
2025-10-23 15:22:02 -0400

    DOM manipulation- Guess my number

--- DOMandEvents-Sec7/GuessMy Number(revision)/script.js
+++ DOMandEvents-Sec7/GuessMy Number(revision)/script.js
@@ -10,7 +10,7 @@ document.querySelector('.score').textContent = 10;
 
 // document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
-*/
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // console.log(secretNumber);
 let score = 20;
@@ -91,3 +91,27 @@ document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('.number').style.width = '15rem';
 
 })
+*/
+
+// Guess My Number
+// Goal: simply guess a secret number which is between 1 and 20. In the input field we type a number, click check button and on the right side of page we get a message(Too low, Too high and Correct number). We start with a score set to 20 and after one failed guess it gets decreased by 1. When the number guessed is same as secret number, we will get "Correct Number" message, screen turns to green and Highscore value will set to the value of Score. If we want to play again simply click on Again! button, the page will get reset to initial values except for Highscore value. Highscore value is kept so if we are quicker to guess the number correct in next round, then the high score will get updated(it will become better).
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
