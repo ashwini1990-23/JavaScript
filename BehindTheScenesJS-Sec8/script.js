419fed4275396da639c4f47eff5281eaa0f7c2a3
Ashwini <ashwiniaski23@gmail.com>
2025-10-30 14:53:32 -0400

    Adding code for Hoisting and TDZ concepts

--- BehindTheScenesJS-Sec8/script.js
+++ BehindTheScenesJS-Sec8/script.js
@@ -35,3 +35,58 @@ const firstName = 'Ashwini';
 calcAge(1991);
 // console.log(age);
 // printAge();
+
+///////////////////////////
+// Hoisting and TDZ
+// Variable hoisting
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Ashwini';
+let job = 'Programmer';
+const year = 1991;
+
+// Functions Hoisting
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example(Hoisting)
+console.log(numberOfProducts);
+if (!numberOfProducts) deleteShoppingCart();
+
+var numberOfProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// diff b/w let const and var
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+///////////////////////////////
+// this keyword
+console.log(this);
+
+const calcAgeNew = function (birthYear) {
+  console.log(2037 - birthYear);
+};
+
+calcAgeNew(1991);
