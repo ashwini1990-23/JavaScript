7c8fae1e9e927b4e731ff3c49bb01badeb132e9e
Ashwini <ashwiniaski23@gmail.com>
2025-11-28 10:03:20 -0500

    Adding README.md file and final code

---
+++ DOMandEvents-Sec7/Modal_Window/README.md
@@ -0,0 +1,4 @@
+Project : Modal Window
+
+Goal: Building UI component- Modal Window
+When user clicks on one of the buttons, a Modal window pops up and overlay which gets overlayed on the rest of the page. To close the modal window we can either click on close button, click on ovraly(outside of the window) or press Esc key on keyboard
--- DOMandEvents-Sec7/Modal_Window/script.js
+++ DOMandEvents-Sec7/Modal_Window/script.js
@@ -1,32 +1,37 @@
 'use strict';
 
+// Selecting elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// Close functionality for Modal Window
 const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
+// Open functionality for Modal Window
 const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
-    btnsOpenModal[i].addEventListener('click', openModal);
+  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-
+// When user clicks close button
 btnCloseModal.addEventListener('click', closeModal);
 
+// When user clicks on overlay
 overlay.addEventListener('click', closeModal);
 
+// When user clicks Esc key
 document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
